4318adece2aafea56eb340d26972310a02bc438a
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-01-24 06:46:25 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,3 +1,5 @@
+from sys import exit
+from importlib import import_module
 from logging import (
     FileHandler,
     StreamHandler,
@@ -5,17 +7,21 @@ from logging import (
     basicConfig,
     error as log_error,
     info as log_info,
+    getLogger,
+    ERROR,
 )
-from os import path as ospath, environ, remove
+from os import path, remove
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from subprocess import run as srun
-from dotenv import load_dotenv, dotenv_values
-from pymongo import MongoClient
 
-if ospath.exists("log.txt"):
+getLogger("pymongo").setLevel(ERROR)
+
+if path.exists("log.txt"):
     with open("log.txt", "r+") as f:
         f.truncate(0)
 
-if ospath.exists("rlog.txt"):
+if path.exists("rlog.txt"):
     remove("rlog.txt")
 
 basicConfig(
@@ -24,55 +30,41 @@ basicConfig(
     level=INFO,
 )
 
-load_dotenv("config.env", override=True)
+settings = import_module("config")
+config_file = {
+    key: value.strip() if isinstance(value, str) else value
+    for key, value in vars(settings).items()
+    if not key.startswith("__")
+}
 
-try:
-    if bool(environ.get("_____REMOVE_THIS_LINE_____")):
-        log_error("The README.md file there to be read! Exiting now!")
-        exit(1)
-except:
-    pass
-
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-if len(BOT_TOKEN) == 0:
+BOT_TOKEN = config_file.get("BOT_TOKEN", "")
+if not BOT_TOKEN:
     log_error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
 
-bot_id = BOT_TOKEN.split(":", 1)[0]
+BOT_ID = BOT_TOKEN.split(":", 1)[0]
 
-DATABASE_URL = environ.get("DATABASE_URL", "")
-if len(DATABASE_URL) == 0:
-    DATABASE_URL = None
-
-if DATABASE_URL is not None:
+if DATABASE_URL := config_file.get("DATABASE_URL", "").strip():
     try:
-        conn = MongoClient(DATABASE_URL)
+        conn = MongoClient(DATABASE_URL, server_api=ServerApi("1"))
         db = conn.mltb
-        old_config = db.settings.deployConfig.find_one({"_id": bot_id})
-        config_dict = db.settings.config.find_one({"_id": bot_id})
-        if old_config is not None:
-            del old_config["_id"]
+        old_config = db.settings.deployConfig.find_one({"_id": BOT_ID}, {"_id": 0})
+        config_dict = db.settings.config.find_one({"_id": BOT_ID})
         if (
-            old_config is not None
-            and old_config == dict(dotenv_values("config.env"))
-            or old_config is None
+            old_config is not None and old_config == config_file or old_config is None
         ) and config_dict is not None:
-            environ["UPSTREAM_REPO"] = config_dict["UPSTREAM_REPO"]
-            environ["UPSTREAM_BRANCH"] = config_dict["UPSTREAM_BRANCH"]
+            config_file["UPSTREAM_REPO"] = config_dict["UPSTREAM_REPO"]
+            config_file["UPSTREAM_BRANCH"] = config_dict["UPSTREAM_BRANCH"]
         conn.close()
     except Exception as e:
         log_error(f"Database ERROR: {e}")
 
-UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "")
-if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = None
+UPSTREAM_REPO = config_file.get("UPSTREAM_REPO", "").strip()
 
-UPSTREAM_BRANCH = environ.get("UPSTREAM_BRANCH", "")
-if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = "master"
+UPSTREAM_BRANCH = config_file.get("UPSTREAM_BRANCH", "").strip() or "master"
 
-if UPSTREAM_REPO is not None:
-    if ospath.exists(".git"):
+if UPSTREAM_REPO:
+    if path.exists(".git"):
         srun(["rm", "-rf", ".git"])
 
     update = srun(
